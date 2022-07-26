d651c8336fed6d26805da2a929dcd55adf356d1d
Lygia Bueno <lybueno@gmail.com>
2022-07-26 17:00:26 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -28,5 +29,10 @@ public class Program {
         for(Seller obj : list ){
             System.out.println(obj);
         }
+
+        System.out.println("=== TEST 4: Seller insert ===");
+        Seller newSeller = new Seller(null, "Greg Geller", "greg@gmail.com", new Date(), 4000.0, new Department(2, null));
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New Id = " + newSeller.getId());
     }
 }
